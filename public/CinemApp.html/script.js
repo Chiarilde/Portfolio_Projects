2c96e7c6131c2a770c053bed79503b0c573d04b8
Chiarilde <chiarilde87@gmail.com>
2024-06-11 17:27:10 +0200

    fixed api key visible

--- public/CinemApp.html/script.js
+++ public/CinemApp.html/script.js
@@ -1,8 +1,6 @@
-import { API_KEY } from "./utils";
-
-const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1`;
+const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=2612f482c74c9b150b89daefdd6d29c4&page=1`;
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
+const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=2612f482c74c9b150b89daefdd6d29c4&query=`;
 const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
